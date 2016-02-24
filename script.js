a6ab0e6dcddc05ccceebbc8dace85ad087c4d995
derzorngottes <amy.conchie@gmail.com>
2016-02-24 13:54:36 -0800

    Complete randomcolors on branch

--- script.js
+++ script.js
@@ -2,27 +2,24 @@ var docBody = document.getElementsByTagName("body")[0];
 
 docBody.setAttribute("style", "text-align:center");
 
-var curCol = true;
-
-function divMaker(curCol) {
+function divMaker() {
   var newDiv = document.createElement('div');
   newDiv.style.boxSizing = "border-box";
   newDiv.style.width = "11.1%";
   newDiv.style.float = "left";
   newDiv.style.paddingBottom = "11.1%";
-  if(curCol === true){
-    newDiv.style.background = "black";
-  }
-  else {
-    newDiv.style.background = "red";
-  }
+  newDiv.style.background = colorPicker();
   docBody.appendChild(newDiv);
+}
+
+function colorPicker() {
+  var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  return color;
 }
 
 function makeBoard(size) {
   for (var i = 0; i < size; i++) {
-    divMaker(curCol);
-    curCol = !curCol;
+    divMaker();
   }
 }
 
