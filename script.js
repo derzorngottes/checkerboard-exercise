9d3bab4ed72a198370b2271b0b76dca5f7ff41ea
derzorngottes <amy.conchie@gmail.com>
2016-02-24 13:38:21 -0800

    Complete first checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Your JS goes here
+var docBody = document.getElementsByTagName("body")[0];
+
+docBody.setAttribute("style", "text-align:center");
+
+var curCol = true;
+
+function divMaker(curCol) {
+  var newDiv = document.createElement('div');
+  newDiv.style.boxSizing = "border-box";
+  newDiv.style.width = "11.1%";
+  newDiv.style.float = "left";
+  newDiv.style.paddingBottom = "11.1%";
+  if(curCol === true){
+    newDiv.style.background = "black";
+  }
+  else {
+    newDiv.style.background = "red";
+  }
+  docBody.appendChild(newDiv);
+}
+
+function makeBoard(size) {
+  for (var i = 0; i < size; i++) {
+    divMaker(curCol);
+    curCol = !curCol;
+  }
+}
+
+makeBoard(63);
