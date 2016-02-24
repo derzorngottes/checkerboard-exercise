83f487013f9ac05b5e3a1e3034125cc67e8a6f8c
derzorngottes <amy.conchie@gmail.com>
2016-02-24 15:30:56 -0800

    Complete gradient on branch

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ var docBody = document.getElementsByTagName("body")[0];
 
 docBody.setAttribute("style", "text-align:center");
 
-function divMaker() {
+function divMaker(grad) {
   var newDiv = document.createElement('div');
   newDiv.style.boxSizing = "border-box";
   newDiv.style.width = "11.1%";
   newDiv.style.float = "left";
   newDiv.style.paddingBottom = "11.1%";
   newDiv.style.background = colorPicker();
+  newDiv.style.opacity = grad;
   docBody.appendChild(newDiv);
 }
 
 function colorPicker() {
   var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+
   return color;
 }
 
 function makeBoard(size) {
-  for (var i = 0; i < size; i++) {
-    divMaker();
+  var grad = 1;
+  for(var rows = 0; rows < 7; rows ++) {
+    grad -= 0.12;
+    for(var cols = 0; cols < 9; cols ++) {
+      divMaker(grad);
+    }
   }
 }
 
